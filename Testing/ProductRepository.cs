8ad7cb4c11c4a27e362ff047663b9f2b51bb1e49
coreyareid <coreyareid@bellsouth.net>
2022-08-04 08:44:14 -0500

    Completed 1st Exercise

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Dapper;
+using System.Data;
+using Testing.Models;
+using System.Collections.Generic;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        IEnumerable<Product> IProductRepository.GetAllProducts()
+        {
+            return _conn.Query <Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
+
